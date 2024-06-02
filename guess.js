55943bb904557237c2e450dec124cbf5bd1058d5
Chaan <rnchagwe27@gmail.com>
2024-06-03 06:24:36 +0800

    if statement

--- guess.js
+++ guess.js
@@ -1,4 +1,15 @@
 const prompt = require("prompt-sync")();
-
-const target = Math.random()
+const target = Math.round(Math.random() * 100)
 console.log(target)
+
+const guess = Number(prompt("Guess the number (0-100): "))
+
+let luck = 0
+
+if (guess > target) {
+    console.log("Quite high.")
+} else if (guess === target) {
+    console.log("Guessed!")
+} else {
+    console.log("Too low.")
+}
