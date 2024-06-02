11b20de54acff813ecb59f320500b1ac73ae2104
Chaan <rnchagwe27@gmail.com>
2024-06-03 06:32:52 +0800

    while loop

--- guess.js
+++ guess.js
@@ -1,15 +1,23 @@
 const prompt = require("prompt-sync")();
 const target = Math.round(Math.random() * 100)
-console.log(target)
 
-const guess = Number(prompt("Guess the number (0-100): "))
+console.log(target)
 
 let luck = 0
 
-if (guess > target) {
-    console.log("Quite high.")
-} else if (guess === target) {
-    console.log("Guessed!")
-} else {
-    console.log("Too low.")
+while(true) {
+    luck++
+    
+    const guess = Number(prompt("Guess the number (0-100): "))
+
+    if (guess > target) {
+        console.log("Quite high.")
+    } else if (guess < target) {
+        console.log("Too low")
+    } else {
+        console.log("Wise guess.")
+        break
+    }
 }
+
+console.log("Caught the luck by", luck, "tries!")
